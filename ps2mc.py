0dc67390a0a000bedb4fbef33a516c1e5092750f
Craig Shaw <craig@craigshaw.com>
2025-06-07 08:31:37 +0100

    Add pylint and fixes for ps2mc.py

--- ps2mc.py
+++ ps2mc.py
@@ -1,3 +1,18 @@
+
+"""
+ps2mc.py - PlayStation 2 Memory Card
+
+This module provides the PS2MC classwith functionality for parsing, validating, and extracting files from Sony PlayStation 2 
+memory card images. It supports reading the superblock, validating the card format, extracting the File Allocation Table (FAT), 
+enumerating all files and directories stored on the card. 
+
+Classes:
+    PS2MC: Main class for interacting with PS2 memory card images.
+    UnsupportedFileTypeError: Exception raised for unsupported or invalid card images.
+
+Usage:
+    Instantiate the PS2MC class with the path to a memory card image file to access its contents and extract files.
+"""
 import array
 import struct
 
@@ -10,9 +25,31 @@ VALID_PAGE_SIZES = [512,1024]
 ECC_SIZE = 16
 
 class UnsupportedFileTypeError(Exception):
-    pass
+    """
+    Exception raised when an unsupported file type is encountered.
+    """
 
 class PS2MC():
+    """
+    Class representing a PlayStation 2 memory card image.
+    Provides methods to read, validate, and extract files from the card image.
+    Attributes:
+        path (str): Path to the memory card image file.
+        img (bytes): Raw image data of the memory card.
+        identifier (str): Identifier string from the superblock.
+        page_size (int): Size of a page in bytes.
+        pages_per_cluster (int): Number of pages per cluster.
+        pages_per_block (int): Number of pages per block.
+        clusters_per_card (int): Total number of clusters on the card.
+        alloc_offset (int): Offset for allocation in the image.
+        alloc_end (int): End offset for allocation in the image.
+        dir_root (int): Root directory cluster number.
+        ifc_table (array.array): Indirect FAT cluster table.
+        cs (int): Cluster size in bytes.
+        ecc_len (int): Length of ECC, if applicable.
+        fat (list[int]): Flattened File Allocation Table.
+        files (list[DirectoryEntry]): List of files and directories on the card.
+    """
     def __init__(self, path):
         self.path = path
 
@@ -21,7 +58,7 @@ class PS2MC():
 
         # Unpack the superblock
         self._unpack_superblock()
-        
+
         # Validate the card
         self._validate()
 
@@ -34,7 +71,7 @@ class PS2MC():
     def __str__(self):
         return f'Card: {self.path}\nSize: {len(self.img)} bytes\nPage size: {self.page_size} bytes\nCluster size: {self.cs} bytes\n' \
             f'Total files: {len(self.files)}\n' + ''.join([f'{f}\n' for f in self.files])
-    
+
     def _unpack_superblock(self):
         sb = struct.unpack('<28s12sHHHHIIIIII8x128s128sBBH', self.img[:SUPERBLOCK_SIZE])
 
@@ -60,9 +97,9 @@ class PS2MC():
             raise UnsupportedFileTypeError("File is not a valid PS2 memory card image")
 
         if self.page_size not in VALID_PAGE_SIZES or \
-           self.page_size == 512 and not (self.pages_per_cluster == 1 or self.pages_per_cluster == 2) or \
+           self.page_size == 512 and not self.pages_per_cluster in (1,2) or \
            self.page_size == 1024 and self.pages_per_cluster != 1:
-               raise UnsupportedFileTypeError("Unsupported memory card geometry")
+            raise UnsupportedFileTypeError("Unsupported memory card geometry")
 
     def _flatten_fat(self):
         fat = []
@@ -85,12 +122,12 @@ class PS2MC():
                         fat.append(struct.unpack_from('<I', fc, fat_entry)[0])
 
         return fat
-    
+
     def _enumerate_all_files(self):
         files=[]
         self._read_files_recursive(files, self.dir_root, '')
         return files
-    
+
     def _read_files_recursive(self, files, cluster_num, path, length=0):
         dirs = self._read_directory_entries(cluster_num, path, length)
         for d in dirs:
@@ -102,7 +139,7 @@ class PS2MC():
     def _read_directory_entries(self, first_cluster_num, path, length):
         rd = self._read_file_starting_at_cluster(first_cluster_num)
         return self._unpack_directory_entries(rd, path, length)
-    
+
     def _read_file_starting_at_cluster(self, cluster_num):
         fat_idx = cluster_num
 
@@ -111,12 +148,12 @@ class PS2MC():
 
         # Now go through the FAT pulling each cluster until we hit an FF entry
         while self.fat[fat_idx] != 0xFFFFFFFF:
-            next_cluster_num = (self.fat[fat_idx] & 0x7FFFFFFF)
+            next_cluster_num = self.fat[fat_idx] & 0x7FFFFFFF
             d += self._read_allocatable_cluster(next_cluster_num)
             fat_idx = next_cluster_num
 
         return d
-    
+
     def _unpack_directory_entries(self, dbuffer, path, length):
         dirs = []
 
@@ -124,48 +161,53 @@ class PS2MC():
             length = struct.unpack_from('<I', dbuffer, 4)[0]
 
         for i in range(length):
-            dir = self._unpack_directory_entry(dbuffer[(i*DIRECTORY_SIZE):(i*DIRECTORY_SIZE)+DIRECTORY_SIZE])
+            d = self._unpack_directory_entry(dbuffer[(i*DIRECTORY_SIZE):(i*DIRECTORY_SIZE)+DIRECTORY_SIZE])
 
-            if dir[7].decode('UTF-8').rstrip('\x00') != '':
-                dirs.append(DirectoryEntry(dir, path))
-            
+            if d[7].decode('UTF-8').rstrip('\x00') != '':
+                dirs.append(DirectoryEntry(d, path))
+
         return dirs
-    
+
     def _unpack_directory_entry(self, b):
         # Expects a 512 byte buffer
         return struct.unpack('<H2xI8sII8sH30x32s416x', b)
-    
+
     def _read_allocatable_cluster(self, offset):
-        o1 = (self.alloc_offset * (self.cs + (self.ecc_len*self.pages_per_cluster))) + (offset * (self.cs + (self.ecc_len*self.pages_per_cluster)))
-        
+        o1 = (self.alloc_offset * (self.cs + (self.ecc_len*self.pages_per_cluster))) + \
+        (offset * (self.cs + (self.ecc_len*self.pages_per_cluster)))
+
         return self._read_cluster_from(o1)
-    
+
     def _read_absolute_cluster(self, cluster_num):
         o1 = (cluster_num * (self.cs + (self.ecc_len*self.pages_per_cluster)))
 
         return self._read_cluster_from(o1)
-    
+
     def _read_cluster_from(self, offset):
         o1 = offset
 
         buffer = bytes()
 
-        for i in range(self.pages_per_cluster):
+        for _ in range(self.pages_per_cluster):
             buffer += self.img[o1:o1+self.page_size]
             o1 += self.page_size + self.ecc_len
 
         return buffer
-    
+
     def _read_file(self, file):
         f = self._read_file_starting_at_cluster(file.cluster)
 
         return f[:file.length]
-    
-    def write_all_to_disk(self, dir):
+
+    def write_all_to_disk(self, directory):
+        """
+        Write all files on the memory card to disk at the specified directory.
+        :param directory: The directory to dump the files to.
+        """
         for f in self.files:
             contents = self._read_file(f)
 
-            path = dir / f.to_path()
+            path = directory / f.to_path()
 
             path.parent.mkdir(parents=True, exist_ok=True)
 
